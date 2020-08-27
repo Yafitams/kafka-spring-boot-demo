3777acda62507ad64e90f275218868354245fc01
Yafit Amshalem <yafitams@gmail.com>
2020-08-27 15:37:21 +0300

    publish text msg to kafka

--- src/main/java/com/example/kafkademo/KafkademoApplication.java
+++ src/main/java/com/example/kafkademo/KafkademoApplication.java
@@ -2,12 +2,20 @@ package com.example.kafkademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkademoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkademoApplication.class, args);
-	}
+	private final KafkaTemplate<String, String> kafkaTemplate;
+
+    KafkademoApplication(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+		this.kafkaTemplate.send("example","msg");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkademoApplication.class, args);
+    }
 
 }
