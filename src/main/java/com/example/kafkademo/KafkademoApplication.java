83ba8d80a5e095253b782a4d0b41c3dc5cf2ec94
Yafit Amshalem <yafitams@gmail.com>
2020-08-27 16:25:44 +0300

    consumer added

--- src/main/java/com/example/kafkademo/KafkademoApplication.java
+++ src/main/java/com/example/kafkademo/KafkademoApplication.java
@@ -1,28 +1,31 @@
 package com.example.kafkademo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class KafkademoApplication {
+public class KafkademoApplication implements CommandLineRunner {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     KafkademoApplication(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
-//        this.kafkaTemplate.send("example", "msg");
-
-        this.kafkaTemplate.send("example", new TestMessage());
     }
 
     public static void main(String[] args) {
         SpringApplication.run(KafkademoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        this.kafkaTemplate.send("example", new TestMessage());
+    }
+
     private static class TestMessage {
         public String msg = "text msg";
-        public String id = "123456";
+        public String id = "12345";
     }
 
 }
