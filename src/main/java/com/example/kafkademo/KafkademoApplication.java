89a2fe1daeac8a0162d956497e7fa7007661884d
Yafit Amshalem <yafitams@gmail.com>
2020-08-27 15:57:59 +0300

    add kafka configuration to send objects

--- src/main/java/com/example/kafkademo/KafkademoApplication.java
+++ src/main/java/com/example/kafkademo/KafkademoApplication.java
@@ -7,15 +7,22 @@ import org.springframework.kafka.core.KafkaTemplate;
 @SpringBootApplication
 public class KafkademoApplication {
 
-	private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    KafkademoApplication(KafkaTemplate<String, String> kafkaTemplate) {
-		this.kafkaTemplate = kafkaTemplate;
-		this.kafkaTemplate.send("example","msg");
+    KafkademoApplication(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+//        this.kafkaTemplate.send("example", "msg");
+
+        this.kafkaTemplate.send("example", new TestMessage());
     }
 
     public static void main(String[] args) {
         SpringApplication.run(KafkademoApplication.class, args);
+    }
+
+    private static class TestMessage {
+        public String msg = "text msg";
+        public String id = "123456";
     }
 
 }
